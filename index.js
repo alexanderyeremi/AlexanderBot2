1016546e5f44d4608a458b450d112cfdb57671e1
alexanderyeremi <80661362+alexanderyeremi@users.noreply.github.com>
2021-04-10 07:27:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ const { removeBackgroundFromImageFile } = require('remove.bg')
 const { nad } = require('./language')
 const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n'
-            + 'FN:FUNKY ID\n' // GANTI NAMA LU
+            + 'FN:Alexander\n' // GANTI NAMA LU
             + 'ORG:OWNER BOTZ;\n'
-            + 'TEL;type=CELL;type=VOICE;waid=6285735054693:+62 857-3505-4693\n' // GANTI NOMOR LU
+            + 'TEL;type=CELL;type=VOICE;waid=6281281783202:+62 812-8178-3202\n' // GANTI NOMOR LU
             + 'END:VCARD'
 // UDAH SEGITU KONSOL KEBAWAH BIARIN AJA
 const ngonsol = JSON.parse(fs.readFileSync('./settings/Ramlan.json'))
